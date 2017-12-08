f5cff355a0fb9a65db4f5fcdf34c8d4d3fbe6751
Adriano Devequi <adrianodevequi@gmail.com>
2017-12-07 23:45:54 -0200

    Cadastro, validação, e exibição funcionando..

---
+++ app/Http/Controllers/TarefasController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Tarefa;
+use Validator;
+
+class TarefasController extends Controller
+{
+    protected function validarTarefa($request){
+        $validator = Validator::make($request->all(),
+        [
+            "titulo" => "required",
+            "descricao" => "required",
+            "status" => "required"
+        ]);
+        return $validator;
+    }
+    
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tarefas = Tarefa::all();
+        return view('tarefas.index', compact('tarefas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('tarefas.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = $this->validarTarefa($request);
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator->errors());
+        }
+        $dados = $request->all();
+        Tarefa::create($dados);
+
+        return redirect()->route('tarefas.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
